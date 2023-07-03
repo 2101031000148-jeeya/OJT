bfaa2bd86493c8620d214339cfdadc0c98673e45
2101031000148-jeeya <137051050+2101031000148-jeeya@users.noreply.github.com>
2023-07-03 15:19:57 +0530

    Add files via upload

---
+++ Day 13/demo.php
@@ -0,0 +1,31 @@
+<!-- <?php
+echo "Hello"
+?> -->
+
+<?php
+$txt = "Jeeya";
+$txt2 = "2101031000148";
+$txt3 = "IT";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $txt2;
+echo "<br>";
+echo $txt3;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+<?php
+$x = 5;
+$y = 4;
+echo "<br>";
+echo $x + $y ;
+?>
+
+
+
---
+++ Day 13/findarea.php
@@ -0,0 +1,14 @@
+
+
+
+<?php
+    $r=5;
+    echo "Area of circle".(3.14*$r*$r);
+?>
+
+<?php  
+ $length = 14;  
+ $width = 12; 
+ echo "<br>"; 
+ echo "area of rectangle is $length * $width= " . ($length * $width) . "<br />";  
+  ?>  
